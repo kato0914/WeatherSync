1dc0bf0ea565da4d088a3edc21e263338127522b
Kato Junichi <katoj62@gmail.com>
2023-09-28 12:03:30 +0900

    second git

--- app.py
+++ app.py
@@ -36,38 +36,31 @@ def scrape_weather():
     # ページのHTMLを取得
         response = requests.get(url)
     # if response.status_code == 200:
-    #     soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.content, 'html.parser')
 
         # 天気情報を抽出
         # city = soup.find('h1', class_='yjw_cityname').text.strip()
         #今日の天気
-        niftty_weather_today_0 = soup.select_one('#todayWeather > .moduleInner > table > tbody > tr + tr > td > img').get('alt')
-        niftty_weather_today_3 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td > img alt')
-        niftty_weather_today_6 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td + td > img alt')
-        niftty_weather_today_9 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td + td + td > img alt')
-        niftty_weather_today_12 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td + td + td + td > img alt')
-        niftty_weather_today_15 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td + td + td + td + td > img alt')
-        niftty_weather_today_18 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td + td + td + td + td + td > img alt')
-        niftty_weather_today_21 = soup.select_one('#todayWeather > table >tbody > tr + tr > td + td + td + td + td + td + td + td > img alt')
+        niftty_weather_today_0 = soup.findAll('img')[6].get('alt')
 
         print(niftty_weather_today_0)
 
         #明日の天気
-        niftty_weather_tomorrow_0 = soup.select_one('#tomorrowWeather > table > tbody > tr + tr > td > img alt')
+        # niftty_weather_tomorrow_0 = soup.select_one('#tomorrowWeather > table > tbody > tr + tr > td > img alt')
         
         # return city, weather, temperature
-        return yahoo_weather_today_0, yahoo_weather_today_3, yahoo_weather_today_6, yahoo_weather_today_9, yahoo_weather_today_12, yahoo_weather_today_15, yahoo_weather_today_18, yahoo_weather_today_21, yahoo_weather_tomorrow_0, niftty_weather_today_0, niftty_weather_today_3, niftty_weather_today_6, niftty_weather_today_9, niftty_weather_today_12, niftty_weather_today_15, niftty_weather_today_18, niftty_weather_today_21, niftty_weather_tomorrow_0
+        return yahoo_weather_today_0, yahoo_weather_today_3, yahoo_weather_today_6, yahoo_weather_today_9, yahoo_weather_today_12, yahoo_weather_today_15, yahoo_weather_today_18, yahoo_weather_today_21, yahoo_weather_tomorrow_0, niftty_weather_today_0
     else:
         return None, None, None
 
 @app.route('/')
 def index():
     # city, yahoo_weather_today_0, temperature = scrape_weather()
-    yahoo_weather_today_0, yahoo_weather_today_3, yahoo_weather_today_6, yahoo_weather_today_9, yahoo_weather_today_12, yahoo_weather_today_15, yahoo_weather_today_18, yahoo_weather_today_21, niftty_weather_today_0, niftty_weather_today_3, niftty_weather_today_6, niftty_weather_today_9, niftty_weather_today_12, niftty_weather_today_15, niftty_weather_today_18, niftty_weather_today_21, niftty_weather_tomorrow_0, yahoo_weather_tomorrow_0 = scrape_weather()
+    yahoo_weather_today_0, yahoo_weather_today_3, yahoo_weather_today_6, yahoo_weather_today_9, yahoo_weather_today_12, yahoo_weather_today_15, yahoo_weather_today_18, yahoo_weather_today_21, niftty_weather_today_0, yahoo_weather_tomorrow_0 = scrape_weather()
     # if city and yahoo_weather_today_0 and temperature:
-    if yahoo_weather_today_0 and yahoo_weather_today_3 and yahoo_weather_today_6 and yahoo_weather_today_9 and yahoo_weather_today_12 and yahoo_weather_today_15 and yahoo_weather_today_18 and yahoo_weather_today_21 and niftty_weather_today_0 and niftty_weather_today_3 and niftty_weather_today_6 and niftty_weather_today_9 and niftty_weather_today_12 and niftty_weather_today_15 and niftty_weather_today_18 and niftty_weather_today_21 and niftty_weather_tomorrow_0 and yahoo_weather_tomorrow_0:
+    if yahoo_weather_today_0 and yahoo_weather_today_3 and yahoo_weather_today_6 and yahoo_weather_today_9 and yahoo_weather_today_12 and yahoo_weather_today_15 and yahoo_weather_today_18 and yahoo_weather_today_21 and niftty_weather_today_0 and yahoo_weather_tomorrow_0:
         # return render_template('weather.html', city=city, yahoo_weather_today_0=yahoo_weather_today_0, temperature=temperature)
-        return render_template('weather.html', yahoo_weather_today_0=yahoo_weather_today_0, yahoo_weather_today_3=yahoo_weather_today_3, yahoo_weather_today_6=yahoo_weather_today_6, yahoo_weather_today_9=yahoo_weather_today_9, yahoo_weather_today_12=yahoo_weather_today_12, yahoo_weather_today_15=yahoo_weather_today_15, yahoo_weather_today_18=yahoo_weather_today_18, yahoo_weather_today_21=yahoo_weather_today_21, niftty_weather_today_0=niftty_weather_today_0, niftty_weather_today_3=niftty_weather_today_3, niftty_weather_today_6=niftty_weather_today_6, niftty_weather_today_9=niftty_weather_today_9, niftty_weather_today_12=niftty_weather_today_12, niftty_weather_today_15=niftty_weather_today_15, niftty_weather_today_18=niftty_weather_today_18, niftty_weather_today_21=niftty_weather_today_21, niftty_weather_tomorrow_0=niftty_weather_tomorrow_0, yahoo_weather_tomorrow_0=yahoo_weather_tomorrow_0)
+        return render_template('weather.html', yahoo_weather_today_0=yahoo_weather_today_0, yahoo_weather_today_3=yahoo_weather_today_3, yahoo_weather_today_6=yahoo_weather_today_6, yahoo_weather_today_9=yahoo_weather_today_9, yahoo_weather_today_12=yahoo_weather_today_12, yahoo_weather_today_15=yahoo_weather_today_15, yahoo_weather_today_18=yahoo_weather_today_18, yahoo_weather_today_21=yahoo_weather_today_21, niftty_weather_today_0=niftty_weather_today_0, yahoo_weather_tomorrow_0=yahoo_weather_tomorrow_0)
     else:
         error_message = "天気情報を取得できませんでした。"
         return render_template('error.html', error_message=error_message)
